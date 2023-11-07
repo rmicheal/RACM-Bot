de37166608daf81ac7629f099647f271892cfd47
Micheal <142172468+rmicheal@users.noreply.github.com>
2023-11-07 20:17:22 +0100

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,45 @@
 module.exports = {
     main: {
         token: process.env.TOKEN,
-        id: "",
-        prefix: "!@#",
+        id: "1084859606897012807",
+        prefix: "-@-",
+    },
+    beta: {
+        server: "873332835426132038",
+    },
+    staff: {
+        support: ["976538165273845830"],
+        management: ["976538165273845830"],
+        supervision: ["976538165273845830"],
+        development: ["976538165273845830"],
+        owner: ["976538165273845830"],
+        staff: ["976538165273845830"],
+    },
+    logs: {
+        error: {
+            token: "VAt2IRNtNUJHJWGOdb-5mRNUQGt8Wwth66AViyVPGPMQS2J0ldcgWg2DUJ8Op8bLa3Rk",
+            id: "1141845152625852549",
+        },
+        guild: {
+            token: "VAt2IRNtNUJHJWGOdb-5mRNUQGt8Wwth66AViyVPGPMQS2J0ldcgWg2DUJ8Op8bLa3Rk",
+            id: "1141845152625852549",
+        },
+    },
+    branding: {
+        name: "RACM",
+        logo: "",
+        owning: "Micheal R",
+    },
+    database: {
+        url: "mongodb+srv://EchoNexus:TLg3rPLq83jsGnXp@echonexus-1.pkms6wk.mongodb.net/",
+        name: "EchoNexus-1",
+        node: "Node 1",
+    },
+    host: {
+        node: "Node 1 - EchoNexus",
+        location: "host.echonexus.pro",
+    },
+    customization: {
+        embedColor: "#ffffff",
     },
 };
